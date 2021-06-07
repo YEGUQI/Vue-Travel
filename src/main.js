9f80b6b8826718c0f18c8deb0e78d5855ae99e09
YGQ-Only <2741827044@qq.com>
2021-06-07 20:57:48 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+// 引入swiper 轮播图插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 样式重置
 import 'style/reset.css'
 
-// inconfont 样式
+// iconfont 样式
 import 'style/iconfont.css'
 
 // 解决移动端 1像素边框问题
 import 'style/border.css'
+// 引入swiper 的样式
+import 'swiper/dist/css/swiper.css'
 
 // 解决移动端点击 3毫秒 延迟的问题
 import fastclick from 'fastclick'
 fastclick.attach(document.body)
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide
+        v-for="item in swiperList"
+        :key="item.id"
+      >
+        <img
+          :src="item.imgUrl"
+          class="swiper-img"
+        >
+      </swiper-slide>
+      <div
+        class="swiper-pagination"
+        slot="pagination"
+      ></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+// >>> 样式穿透 只要 wrapper下面的子组件中出现 swiper-pagination-bullet-active 都生效
+.wrapper >>> .swiper-pagination-bullet-active
+  background-color: #fff;
+.wrapper
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.2%;
+  background-color: #eee;
+  .swiper-img
+    width: 100%;
+</style>
