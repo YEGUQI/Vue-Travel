66b7759efc43546fc695232646908b4ba859140f
YGQ-Only <2741827044@qq.com>
2021-06-12 11:17:15 +0800

    add search to city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Search">
+    <input
+      type="text"
+      class="search-input"
+      placeholder="请输入城市名或拼音"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~style/varibles.styl';
+.Search
+  height: 0.72rem;
+  background-color: $bgColor;
+  padding: 0 0.1rem;
+  .search-input
+    box-sizing: border-box;
+    width: 100%;
+    height: 0.62rem;
+    text-align: center;
+    border-radius: 0.06rem;
+    color: #666;
+    padding: 0 0.1rem;
+</style>
